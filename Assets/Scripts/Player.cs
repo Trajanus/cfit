91db68bc8a648a108f043c564e5c160270a050f6
Trajanus <derektgoodwin@gmail.com>
2021-06-02 11:31:38 -0700

    Added default folders, Player script, external library for shared unity code, and defined the throttle Axis.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityLibrary;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float throttle = Input.GetAxisRaw(InputAxisNames.Throttle);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(100 * throttle, 0);
+
+        float verticalAxis = Input.GetAxisRaw(InputAxisNames.Vertical);
+
+        if (verticalAxis > 0)
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (verticalAxis < 0)
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
---
+++ Assets/Scripts/UnityLibrary/InputAxisNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UnityLibrary
+{
+    public static class InputAxisNames
+    {
+        public static string Throttle = "Throttle";
+        public static string Vertical = "Vertical";
+    }
+}
