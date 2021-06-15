465ffb71f0cce4c6195eb91ed2beaf55cae9c1ac
Trajanus <derektgoodwin@gmail.com>
2021-06-15 13:03:14 -0700

    Player movment now respects rotation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityLibrary;
@@ -7,10 +8,12 @@ public class Player : MonoBehaviour
 {
     public int speed;
 
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,9 +25,6 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         float throttle = Input.GetAxisRaw(InputAxisNames.Throttle);
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(speed * throttle, 0);
-
         float verticalAxis = Input.GetAxisRaw(InputAxisNames.Vertical);
 
         if (verticalAxis > 0)
@@ -35,5 +35,23 @@ public class Player : MonoBehaviour
         {
             transform.Rotate(Vector3.back);
         }
+
+        float acceleration = throttle * speed;
+
+        Vector3 throwaway = new Vector3();
+        float angle;
+        transform.rotation.ToAngleAxis(out angle, out throwaway);
+
+        if (transform.rotation.z < 0)
+            angle *= -1;
+
+        float radianAngle = (float)(Math.PI / 180) * angle;
+
+        Vector2 accelerationVector = new Vector2(Mathf.Cos(radianAngle), Mathf.Sin(radianAngle));
+
+        accelerationVector.x = (accelerationVector.x * acceleration) - (rb2d.velocity.x * 2);
+        accelerationVector.y = (accelerationVector.y * acceleration) - (rb2d.velocity.y * 2);
+
+        rb2d.AddForce(accelerationVector);
     }
 }
