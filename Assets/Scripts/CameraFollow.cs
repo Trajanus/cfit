34e6372d1d9baa443274f5fb04f4d5fe9f7e3f20
Trajanus <derektgoodwin@gmail.com>
2021-06-28 15:36:19 -0700

    Tied camera follow to the target objects velocity to help prevent the camera from lagging too far behind the object.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float smoothness;
-    public Transform targetObject;
+    public GameObject targetObject;
+
     private Vector3 initalOffset;
     private Vector3 cameraPosition;
+    private Rigidbody2D targetRb2d;
+    
 
     void Start()
     {
-        initalOffset = transform.position - targetObject.position;
+        initalOffset = transform.position - targetObject.transform.position;
+        targetRb2d = targetObject.GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        cameraPosition = targetObject.position + initalOffset;
+        cameraPosition = targetObject.transform.position + initalOffset + new Vector3(targetRb2d.velocity.x / smoothness, targetRb2d.velocity.y / smoothness, 0);
         transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness * Time.fixedDeltaTime);
     }
 }
